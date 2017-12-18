14058f1ec9dede7c363e2d0dfae0eea1709d5ada
licheng <lc900508@sina.com>
2017-12-18 18:40:55 +0800

    commit

--- ToolSwift/ToolSwift/Classes/Index/Controller/LLRankListViewController.swift
+++ ToolSwift/ToolSwift/Classes/Index/Controller/LLRankListViewController.swift
@@ -10,15 +10,67 @@ import UIKit
 
 class LLRankListViewController: LLBaseViewController {
 
+    private var rankList = [RankingModel]()
+    
+    private lazy var tableView: UITableView = {
+        let tw = UITableView(frame: .zero, style: .plain)
+        tw.backgroundColor = UIColor.background
+        tw.tableFooterView = UIView()
+        tw.separatorStyle = .none
+        tw.delegate = self
+        tw.dataSource = self
+        tw.separatorStyle = .none
+        tw.register(cellType: LLRankTCell.self)
+        tw.MJHead = LLRefreshHeader{ self.loadData() }
+        tw.uempty = LLEmptyView { self.loadData() }
+        return tw
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        loadData(true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc private func loadData(_ showloading: Bool = false) {
+        
+        ApiToolProvider(showloading).request(ApiTool.rankList, model: RankinglistModel.self) { (returnData) in
+            self.tableView.MJHead.endRefreshing()
+            self.tableView.uempty?.allowShow = true
+            
+            self.rankList = returnData?.rankinglist ?? []
+            self.tableView.reloadData()
+        }
     }
-
+    
+    override func configUI() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints{ $0.edges.equalTo(self.view.usnp.edges) }
+    }
 }
+
+extension LLRankListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rankList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(for: indexPath, cellType: LLRankTCell.self)
+        cell.model = rankList[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return screenWidth * 0.4
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let model = rankList[indexPath.row]
+        let vc = LLComicListViewController(argCon: model.argCon,
+                                          argName: model.argName,
+                                          argValue: model.argValue)
+        vc.title = "\(model.title!)榜"
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
--- ToolSwift/ToolSwift/Classes/Index/Controller/LLSubscibeListViewController.swift
+++ ToolSwift/ToolSwift/Classes/Index/Controller/LLSubscibeListViewController.swift
@@ -10,26 +10,110 @@ import UIKit
 
 class LLSubscibeListViewController: LLBaseViewController {
 
+    private var subscribeList = [ComicListModel]()
+    
+    private lazy var collectionView: UICollectionView = {
+        let lt = LLCollectionViewSectionBackgroundLayout()
+        lt.minimumInteritemSpacing = 5
+        lt.minimumLineSpacing = 10
+        let cw = UICollectionView(frame: CGRect.zero, collectionViewLayout: lt)
+        cw.backgroundColor = UIColor.background
+        cw.delegate = self
+        cw.dataSource = self
+        cw.alwaysBounceVertical = true
+        cw.register(cellType: LLComicCCell.self)
+        cw.register(supplementaryViewType: LLComicCHeadView.self, ofKind: UICollectionElementKindSectionHeader)
+        cw.register(supplementaryViewType: LLComicCFootView.self, ofKind: UICollectionElementKindSectionFooter)
+        cw.MJHead = LLRefreshHeader{ self.loadData() }
+        cw.MJFoot = LLRefreshTipKissFooter(with: "使用妖气币可以购买订阅漫画\nVIP会员购买还有优惠哦~")
+        cw.uempty = LLEmptyView { self.loadData() }
+        return cw
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        loadData(true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func loadData(_ showloading: Bool = false) {
+        
+        ApiToolProvider(showloading).request(ApiTool.subscribeList, model: SubscribeListModel.self) { (returnData) in
+            self.collectionView.MJHead.endRefreshing()
+            self.collectionView.uempty?.allowShow = true
+            
+            self.subscribeList = returnData?.newSubscribeList ?? []
+            self.collectionView.reloadData()
+        }
     }
-    */
-
+    
+    override func configUI() {
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints{ $0.edges.equalTo(self.view.usnp.edges) }
+    }
 }
+
+extension LLSubscibeListViewController: LLCollectionViewSectionBackgroundLayoutDelegateLayout, UICollectionViewDataSource{
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return subscribeList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, backgroundColorForSectionAt section: Int) -> UIColor {
+        return UIColor.white
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let comicList = subscribeList[section]
+        return comicList.comics?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionElementKindSectionHeader {
+            let head = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, for: indexPath, viewType: LLComicCHeadView.self)
+            let comicList = subscribeList[indexPath.section]
+            head.iconView.kf.setImage(urlString: comicList.titleIconUrl)
+            head.titleLabel.text = comicList.itemTitle
+            head.moreButton.isHidden = !comicList.canMore
+            head.moreActionClosure { [weak self] in
+                let vc = LLComicListViewController(argCon: comicList.argCon, argName: comicList.argName, argValue: comicList.argValue)
+                vc.title = comicList.itemTitle
+                self?.navigationController?.pushViewController(vc, animated: true)
+            }
+            return head
+        } else {
+            let foot = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionFooter, for: indexPath, viewType: LLComicCFootView.self)
+            return foot
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        let comicList = subscribeList[section]
+        return comicList.itemTitle?.count ?? 0 > 0 ? CGSize(width: screenWidth, height: 44) : CGSize.zero
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return subscribeList.count - 1 != section ? CGSize(width: screenWidth, height: 10) : CGSize.zero
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(for: indexPath, cellType: LLComicCCell.self)
+        cell.style = .withTitle
+        let comicList = subscribeList[indexPath.section]
+        cell.model = comicList.comics?[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = floor(Double(screenWidth - 10.0) / 3.0)
+        return CGSize(width: width, height: 240)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let comicList = subscribeList[indexPath.section]
+        guard let model = comicList.comics?[indexPath.row] else { return }
+        let vc = LLComicViewController(comicid: model.comicId)
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
